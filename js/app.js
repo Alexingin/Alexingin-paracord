cd3e855f3239a8e77daf6f39e6d86888872d270e
Alexingin <91128371+Alexingin@users.noreply.github.com>
2023-07-26 02:53:52 +0300

    js

--- js/app.js
+++ js/app.js
@@ -12,3 +12,17 @@ document.querySelectorAll('.slider').forEach((n, i) => {
 	})
 })
 bindSwipers(slider1, slider2, slider3, slider4)
+
+  // Находим все элементы с классом "imageContainer"
+  const imageContainers = document.querySelectorAll('.imageContainer');
+
+  // Добавляем обработчик события на клик для каждого элемента
+  imageContainers.forEach(imageContainer => {
+    imageContainer.addEventListener('click', function() {
+      // Получаем URL сайта из атрибута данных "data-website"
+      const url = imageContainer.dataset.website;
+
+      // Открываем новую вкладку с указанным URL
+      window.open(url, '_blank');
+    });
+  });
